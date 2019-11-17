d9d7e9437f73c7f5ac028b7f15de96b3c9ab1b7b
Joe <kaalm1@gmail.com>
2019-11-17 09:00:19 -0800

    more tests

--- test_queue_stack.rb
+++ test_queue_stack.rb
@@ -16,9 +16,27 @@ class TestSimpleNumber < Test::Unit::TestCase
     queue.pop
     queue.pop
     assert_equal([15,20,40,10],  queue.list)
-    
+
     assert_equal(40,  queue.max)
     # assert_equal(6, QueueStack.new(2).multiply(3) )
+
+    queue.push(10)
+    queue.push(10)
+    queue.push(15)
+    queue.push(20)
+    queue.push(20)
+    queue.push(10)
+
+    assert_equal([15,20,40,10,10,10,15,20,20,10],  queue.list)
+    assert_equal(40,  queue.max)
+
+    queue.pop
+    queue.pop
+    queue.pop
+
+    assert_equal([10,10,10,15,20,20,10],  queue.list)
+    assert_equal(20,  queue.max)
+
   end
 
 end
